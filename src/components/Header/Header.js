b1973e25bbe3b7315b21d04aa5b421c41321cf8a
Mihai Zofei VM <mihaizofei@yahoo.com>
2018-03-23 07:38:03 +0000

    Finished the header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import AppBar from 'material-ui/AppBar';
+import {white} from 'material-ui/styles/colors';
+import NavigationRefresh from 'material-ui/svg-icons/navigation/refresh';
+import ContentAdd from 'material-ui/svg-icons/content/add';
 
-const style = {
-    height: 50,
-    backgroundColor: '#3F51B5',
-    paddingBottom: 60
-  };
+import './Header.css';
 
-const Header = () => (
-    <div>
-        <AppBar style={style}
-            title="WeatherZ"
-            showMenuIconButton={false}
-            iconClassNameRight="muidocs-icon-navigation-expand-more"
-  />
-    </div>
-);
+class Header extends Component {
+    render() {
+        const rightButtons = (
+            <div className="icon-style">
+                <NavigationRefresh color={white}/>
+                <ContentAdd color={white} />
+            </div>
+        );
+
+        return (<div>
+            <AppBar className="bar-style"
+                title="WeatherZ"
+                showMenuIconButton={false}
+                iconElementRight={rightButtons}
+            />
+        </div>);
+    }
+}
 
 export default Header;
